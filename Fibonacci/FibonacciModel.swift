024cdfc37ec62262644fe574ac3c3587acf5ea44
James Salame <jimmy@salame.us>
2014-09-14 18:42:25 -0400

    first commit

---
+++ Fibonacci/FibonacciModel.swift
@@ -0,0 +1,27 @@
+//
+//  FibonacciModel.swift
+//  Fibonacci
+//
+//  Created by James Salame on 9/14/14.
+//  Copyright (c) 2014 Salame. All rights reserved.
+//
+
+import Foundation
+
+class FibonacciModel {
+    
+    func calculateFibonacciNumbers (minimum2 endOfSequence:Int) -> Array<Int> {
+        
+        //Declaring a stored property with initial values
+        var sequence : [Int] = [1,1]
+        
+        for number in 2..<endOfSequence {
+            
+            var newFibonacciNumber = sequence[number-1] + sequence[number-2]
+            sequence.append(newFibonacciNumber)
+        }
+        
+        return sequence
+    }
+    
+}
--- Fibonacci/ViewController.swift
+++ Fibonacci/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    //Graphic elements
+    var selectedValueLabel: UILabel?
+    var outputTextView: UITextView?
+    var theSlider: UISlider?
+    
+    //An instance of the model
+    var fibo: FibonacciModel = FibonacciModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.addASlider()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func addASlider(){
+        
+        //Create the slider
+        self.theSlider = UISlider(frame: CGRectMake(60, 244, 200, 20))  // explain the unwrapping..
+        self.view.addSubview(theSlider!)
+        
+        //Assign maximum and minimum value
+        self.theSlider!.minimumValue = 2
+        self.theSlider!.maximumValue = 92
+        
+        //Create labels to display the min/max values
+        var minLabel = UILabel(frame: CGRectMake(70, 205, 40, 40))
+        minLabel.text = String (Int(theSlider!.minimumValue)) //do we have to conver a float to int before converting to string?
+        self.view.addSubview(minLabel)
+        
+        var maxLabel = UILabel(frame: CGRectMake(235, 205, 40, 40))
+        maxLabel.text = String (Int(theSlider!.maximumValue))
+        self.view.addSubview(maxLabel)
+        
+        //Create labels to display selected value
+        self.selectedValueLabel = UILabel(frame: CGRectMake(140, 320, 40, 40))
+        self.selectedValueLabel!.text = String(Int(theSlider!.value))
+        self.view.addSubview(selectedValueLabel!) //set to minimum value
+        
+        //Create a label to hold the output
+        self.outputTextView = UITextView(frame: CGRectMake(20, 350, 300, 140))
+        self.view.addSubview(self.outputTextView!)
+        
+        //Update the value label when the slider value is updated.
+        self.theSlider!.addTarget(self, action: "sliderValueDidChange", forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func sliderValueDidChange () {
+        
+        var returnedArray: [Int] = []
+        var formattedOutput:String = ""
+        
+        //Display the updated slider value
+        self.selectedValueLabel!.text = String(Int(theSlider!.value))
+        
+        //Calculate the Fibonacci elements based on the new slider value
+        returnedArray = self.fibo.calculateFibonacciNumbers(minimum2: Int(theSlider!.value))
+        
+        //Put the elements in a nicely formatted array
+        for number in returnedArray {
+            
+            formattedOutput = formattedOutput + String(number) + ", "
+        }
+        
+        //Update the textfield with the formatted array
+        self.outputTextView!.text = formattedOutput
+    }
 
 }
 
